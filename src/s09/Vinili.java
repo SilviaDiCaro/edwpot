701edc682a38af6d2ef7e336b514309c2fa0cad2
Administrator <Administrator@192.168.12.51>
2019-08-30 17:59:36 +0200

    Esercizio vinili

--- src/s09/Vinili.java
+++ src/s09/Vinili.java
@@ -2,10 +2,7 @@ package s09;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.Duration;
-import java.time.LocalTime;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,57 +10,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/s09/greeter")
+@WebServlet("/s09/vinili")
 public class Vinili extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        HttpSession session = request.getSession();
+       HttpSession session = request.getSession();
+ 
+           {  
+             response.setContentType("text/html"); 
+             PrintWriter pwriter=response.getWriter(); 
 
-        LocalTime start = (LocalTime) session.getAttribute("start"); //getAttribute per vedere se c'è start
-        Duration duration; //durata, package javatime
-        if (start == null) {
-            duration = Duration.ZERO; //inizializzo durata a 0.classe Duration.proprietà pubblica della classe (è final, costante che rappresenta la durata zero)
-            session.setAttribute("start", LocalTime.now());//se start è null, è la prima volta che lo cerco e faccio una setAttribute start a cui associo un localtime.now, se c'è la uso.
-        } else {//se start non c'è, lo crea e lo mette
-            duration = Duration.between(start, LocalTime.now());//metodo between tra start (che è la mia local time) e now (trova l'arco temporale)
-        }
-//done è nell'index
-        if (request.getParameter("done") != null) {//chiamo il metodo getparameter. se il chiamante ha specificato il parametro "done", verifico se è diverso da null 
-            session.invalidate();//termino la sessione
-
-            response.setContentType("text/html");
-            response.setCharacterEncoding("utf-8");
-            try (PrintWriter writer = response.getWriter()) {
-                writer.println("<!DOCTYPE html><html>");
-                writer.println("<head><meta charset=\"utf-8\">");
-                writer.println("<title>So long</title></head>");
-                writer.println("<body><h1>Goodbye</h1>");
-                writer.println("<p>You worked on this stuff for " + duration.getSeconds() + " seconds</p>");
-                writer.println("</body></html>");
-            }
-            
-        } else {
-
-        request.setAttribute("duration", duration);//prendo duration la metto in request come attributo
-        RequestDispatcher rd = request.getRequestDispatcher("/s09/greeter.jsp");//passo il controllo a un'altra servlet s09/greeter.jsp
-        rd.forward(request, response);
-    }
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
+             String name = request.getParameter("uname");
+             String age = request.getParameter("uage");
+             pwriter.println("Name: "+name); 
+             pwriter.println("Age: "+age); 
+             pwriter.close(); 
+          }
     }
 }
-
-//
-//
-//LocalTime st;
-//Object o = session.getAttribute("start");
-//if(o instanceof LocalTime) {
-//	st = (LocalTime)o;
-//}else
-//}
-
